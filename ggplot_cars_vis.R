ff83ef59a7135f188cc7b0e286a5506ba9033763
nshedd <58157630+nshedd@users.noreply.github.com>
2021-02-07 15:26:26 -0500

    Add files via upload

---
+++ ggplot_cars_vis.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(ggiraph)
+
+cars = read.csv("cars-sample.csv", header=TRUE, row.names=1)
+
+cars$Make.Model = paste(cars$Manufacturer, cars$Car, sep=" ")
+head(cars)
+
+print(cars$Car)
+
+plot = ggplot(cars, aes(x=Weight, y=MPG, color=Manufacturer)) + geom_point_interactive(alpha=0.5, aes(size=Weight, tooltip = Make.Model, data_id = Make.Model))
+
+girafe(ggobj = plot)
+
